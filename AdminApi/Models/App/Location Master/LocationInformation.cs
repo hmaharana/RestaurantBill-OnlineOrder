ea59033bacdbb6cad6283e5ebf3675ebf79c335c
Dwitikrushna <dwitikrushna.pradhan91@gmail.com>
2024-02-15 15:22:30 +0530

    dsfgdsfgdf

--- AdminApi/Models/App/Location Master/LocationInformation.cs
+++ AdminApi/Models/App/Location Master/LocationInformation.cs
@@ -4,7 +4,7 @@ namespace AdminApi.Models.App.Location_Master
 {
     public class LocationInformation
     {  
-        public int LocationInformationId { get; set; }
+        public int LocationInformationId { get; set; } 
         public double Latitude { get; set; } 
         public double Longitude { get; set; }
         public int CreatedBy { get; set; }
